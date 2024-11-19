80d1aad7c0bfc66faa0289ce1b38b516baf125d1
gabriel cordeiro <ogabrieldscordeiro@gmail.com>
2024-11-18 21:19:41 -0300

    Desafio Classificador Heroi

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,19 @@
+nome = str(input("Digite seu nome: "))
+xp = int(input("Digite seu XP: "))
+if xp <=1000:
+    print('Ferro')
+elif xp >1000 and xp <2001:
+    print('Bronze')
+elif xp >2000 and xp <5001:
+    print('Prata')
+elif xp > 5000 and xp <7001:
+    print('Ouro')
+elif xp > 7000 and xp <8001:
+    print('Platina')
+elif xp > 8000 and xp <9001:
+    print('Ascendente')
+elif xp > 9000 and xp <10001:
+    print('Imortal')
+else:
+    print('Radiante')
+print("O Herói de nome",nome,"está no nível de", xp,"XP")
